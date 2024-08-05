152d9d3f19e4f4a8fc60ffbb80be84f9dcf3dcb9
Radymyr <loboda.radymyr@gmail.com>
2024-08-05 16:11:51 +0300

    добавлены логи в опен аи

--- src/openai.js
+++ src/openai.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import OpenAI from 'openai';
 import { sendPhotoToUser } from './bot.js';
+import fs from 'fs/promises';
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
